97de837e8083efd2a5bf8533ce1f050687375173
Andrii Malnov <andrey.malnov@gmail.com>
2021-07-28 19:24:47 +0300

    Initial Commit

---
+++ ChooseTheFlagGame/ChooseTheFlagGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChooseTheFlagGameApp.swift
+//  ChooseTheFlagGame
+//
+//  Created by Mac on 28.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct ChooseTheFlagGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
